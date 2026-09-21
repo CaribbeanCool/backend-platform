a6051fa37cada4312d43559833d14cf68e1cc25b
CaribbeanCool <alejandro.perez21@upr.edu>
2026-09-21 14:07:22 -0400

    Reverting Failing CI

--- tests/integration/test_health.py
+++ tests/integration/test_health.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 def test_health(client):
     response = client.get("/health")
 
-    assert response.status_code == 500
+    assert response.status_code == 200
     assert response.json() == {
         "status": "ok",
     }
